fbfc862734b4b5df0773e66c30acc4565e5d0049
aleksarki <sashas200@yandex.ru>
2025-03-12 01:17:27 +0500

    frontend: added repo link to footer

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import "./App.scss";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import TwoPanels from "./components/TwoPanels";
+
+function App() {
+  return (
+    <div className="App">
+      <Header pageTitle="Каталог" pageIndex="0" />
+      <TwoPanels />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,7 +3,8 @@ import './Footer.scss';
 function Footer() {
     return (
         <div className="Footer">
-            <span className="biblio">Библиотик</span>
+            <span className="biblio">Библиотик. 2025</span>
+            <a href="https://github.com/aleksarki/bibliotic" className="repo">Gitbub Repo</a>
         </div>
     );
 }
