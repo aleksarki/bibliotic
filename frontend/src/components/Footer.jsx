a7504cd53ca3dce16cfae87e81388efb6f5bb1f0
aleksarki <sashas200@yandex.ru>
2025-04-04 20:57:28 +0500

    frontend: put the buttons on the footer inside a box

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,8 +2,9 @@
  * Footer with some info.
  */
 
-import Button, { buttonColors } from './ui/Button';
-import { themes, useTheme } from '../contexts/ThemeContext';
+import Button, { buttonColors } from "./ui/Button";
+import ButtonBox from "./ui/ButtonBox";
+import { themes, useTheme } from "../contexts/ThemeContext";
 
 import './Footer.scss';
 
@@ -15,9 +16,12 @@ function Footer() {
             <span className="app-title">Библиотик. 2025</span>
             <a href="https://github.com/aleksarki/bibliotic" className="app-repo">Gitbub Repo</a>
             { /* temporary solution */ }
-            <Button text="Light" style={ buttonColors.BLUE } onClick={ () => setTheme(themes.LIGHT) } />
-            <Button text="Dark" style={ buttonColors.BLUE } onClick={ () => setTheme(themes.DARK) } />
-            <Button text="Skeuo" style={ buttonColors.BLUE } onClick={ () => setTheme(themes.SKEUO) } />
+            <span>Установить тему:</span>
+            <ButtonBox gap={ 10 } buttons={ [
+                <Button text="Light" style={ buttonColors.GREEN } onClick={ () => setTheme(themes.LIGHT) } />,
+                <Button text="Dark" style={ buttonColors.BLUE } onClick={ () => setTheme(themes.DARK) } />,
+                <Button text="Skeuo" style={ buttonColors.SKEUO } onClick={ () => setTheme(themes.SKEUO) } />
+            ] } />
         </div>
     );
 }
