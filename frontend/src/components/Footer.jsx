2a3f0d06bff962c027c08422062eac4726e01ec2
aleksarki <sashas200@yandex.ru>
2025-04-07 02:35:13 +0500

    frontend: implemented options page

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,26 +2,14 @@
  * Footer with some info.
  */
 
-import Button, { buttonColors } from "./ui/Button";
-import ButtonBox from "./ui/ButtonBox";
-import { themes, useTheme } from "../contexts/ThemeContext";
-
 import './Footer.scss';
 
 function Footer() {
-    const {theme, setTheme} = useTheme();
 
     return (
         <div className="Footer">
             <span className="app-title">Библиотик. 2025</span>
             <a href="https://github.com/aleksarki/bibliotic" className="app-repo">Gitbub Repo</a>
-            { /* temporary solution */ }
-            <span>Установить тему:</span>
-            <ButtonBox gap={ 10 } buttons={ [
-                <Button text="Light" style={ buttonColors.GREEN } onClick={ () => setTheme(themes.LIGHT) } />,
-                <Button text="Dark" style={ buttonColors.BLUE } onClick={ () => setTheme(themes.DARK) } />,
-                <Button text="Skeuo" style={ buttonColors.SKEUO } onClick={ () => setTheme(themes.SKEUO) } />
-            ] } />
         </div>
     );
 }
--- frontend/src/views/OptionsView.jsx
+++ frontend/src/views/OptionsView.jsx
@@ -2,17 +2,40 @@
  * View for managing the apllication options.
  */
 
+import Button, { buttonColors } from "../components/ui/Button";
+import ButtonBox from "../components/ui/ButtonBox";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import TwoPanels from "../components/TwoPanels";
+import { themes, useTheme } from "../contexts/ThemeContext";
 
 import "./OptionsView.scss";
 
 function OptionsView() {
+    const {theme, setTheme} = useTheme();
+
     return (
         <div className="OptionsView">
             <Header pageTitle="Настройки" pageIndex="2" />
-            <TwoPanels />
+            <TwoPanels left={
+                <div className="left-panel">
+                    <div className="option-row">
+                        <span>Тема оформления</span>
+                        <ButtonBox gap={ 10 } buttons={ [
+                            <Button text="Светлая" style={ buttonColors.GREEN } onClick={ () => setTheme(themes.LIGHT) } />,
+                            <Button text="Тёмная" style={ buttonColors.BLUE } onClick={ () => setTheme(themes.DARK) } />,
+                            <Button text="Скевоморф" style={ buttonColors.SKEUO } onClick={ () => setTheme(themes.SKEUO) } />
+                        ] } />
+                    </div>
+                    <div className="option-row">
+                        <span>Аккаунт</span>
+                        <ButtonBox gap={ 10 } buttons={ [
+                            <Button text="Сменить пароль" style={ buttonColors.YELLOW } />,
+                            <Button text="Выйти" style={ buttonColors.RED } />
+                        ] } />
+                    </div>
+                </div>
+            } />
             <Footer />
         </div>
     );
