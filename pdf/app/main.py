d94d194649b57d671c08ba8eb336cc21fec8dca6
aleksarki <sashas200@yandex.ru>
2025-04-07 18:36:44 +0500

    backend: added endpoint for text extraction

--- pdf/app/main.py
+++ pdf/app/main.py
@@ -1,5 +1,6 @@
 
-from fastapi import FastAPI
+from fastapi import FastAPI, File, HTTPException, UploadFile
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
@@ -8,3 +9,18 @@ def hello():
     """ Test function.
     """
     return {"message": "hello there"}
+
+@app.post('/pdf/extract-text')
+async def extractText(file: UploadFile = File(...)):
+    if not file.filename.endswith(".pdf"):
+        raise HTTPException(status_code=400, detail="Not a pdf file.")
+    
+    # todo: implement logics
+    
+    return JSONResponse(
+        status_code=200,
+        content={
+            "file": file.filename,
+            "text": "stub text"
+        }
+    )
